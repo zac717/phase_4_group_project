6ef53fc9ac966b83d9aadcef67676b557ed5de94
jamil <jamil.saleh@student.moringaschool.com>
2023-03-28 20:22:08 +0300

    containers added

---
+++ client/containers/search/Search.js
@@ -0,0 +1,39 @@
+import * as React from 'react'
+import { Grid } from '@mui/material'
+import ListResults from './ListResults'
+import BookResults from './BookResults'
+
+const Search = ({
+  recommendationLists,
+  currentList,
+  handleListSearch,
+  bookResults,
+  handleBookSearch,
+  handleFetchBook,
+  loading,
+}) => {
+  return (
+    <Grid container maxWidth='xl' spacing={6}>
+      <Grid item sx={{ width: '100%' }}>
+        <BookResults
+          books={bookResults}
+          handleBookSearch={handleBookSearch}
+          handleFetchBook={handleFetchBook}
+          loading={loading}
+        />
+      </Grid>
+
+      {recommendationLists && (
+        <ListResults
+          currentList={currentList}
+          recommendationLists={recommendationLists}
+          handleListSearch={handleListSearch}
+          handleFetchBook={handleFetchBook}
+          loading={loading}
+        />
+      )}
+    </Grid>
+  )
+}
+
+export default Search
