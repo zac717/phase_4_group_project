c6c72d80d3493ebb0f25fe813a29ef495d65d327
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-03-29 14:30:48 +0300

    UI Setup

--- client/src/containers/Nav.js
+++ client/src/containers/Nav.js
@@ -26,7 +26,7 @@ const Nav = ({ user, onLogout }) => {
             className='flex'
             align='center'>
             <BookmarkIcon sx={{ fontSize: '3em' }} color='primary' />
-            <span className='logo'>Best Books</span>
+            <span className='logo'>Page Turner</span>
           </Typography>
         </Link>
         {!user ? (
