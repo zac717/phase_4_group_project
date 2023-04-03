7fff0d82901202542273261a1e02bf1d7d45c30d
Isaac Ongera <isaacongera717@gmail.com>
2023-04-03 11:22:15 +0300

    ative record production warning

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module ThePageTurners
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.active_record.sqlite3_production_warning = false
 
     # Configuration for the application, engines, and railties goes here.
     #
